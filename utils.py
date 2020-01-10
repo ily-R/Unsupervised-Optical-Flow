a62ad062bc4356c05efadf9347daed7361b9461e
ily-R <arouiilyes@gmail.com>
2020-01-10 03:04:33 +0100

    weights bug

--- utils.py
+++ utils.py
@@ -204,8 +204,8 @@ def EPE(flow_pred, flow_true, real=False):
 
 def EPE_all(flows_pred, flow_true, weights=(0.005, 0.01, 0.02, 0.08, 0.32)):
 
-    if weights is None:
-        weights = [1 / len(flows_pred)] * len(flows_pred)
+    if len(flows_pred) < 5:
+        weights = [0.005]*len(flows_pred)
     loss = 0
 
     for i in range(len(weights)):

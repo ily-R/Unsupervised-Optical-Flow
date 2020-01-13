1a27cfa73995cd55d4fd9d621cb472f5201a3dd6
ilyasAr <arouiilyes@gmail.com>
2020-01-13 23:10:07 +0100

    testing predict

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import argparse
 from dataset import *
 from models import FlowNetS, Unsupervised, LightFlowNet, PWC_Net
 import matplotlib.pyplot as plt
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 if __name__ == '__main__':
 
@@ -28,7 +29,7 @@ if __name__ == '__main__':
         path = type(mymodel).__name__
 
     os.makedirs(os.path.join("result", path), exist_ok=True)
-    mymodel.load_state_dict(torch.load(os.path.join("model_weight", path, 'best_weight.pt'))['model_state_dict'])
+    mymodel.load_state_dict(torch.load(os.path.join("model_weight", path, 'best_weight.pt'), map_location=device)['model_state_dict'])
     if args.unsup:
         mymodel = mymodel.predictor
     mymodel.eval()
@@ -36,7 +37,7 @@ if __name__ == '__main__':
         albu.Normalize((0., 0., 0.), (1., 1., 1.)),
         ToTensor()
     ])
-    for i in range(0, len(file_names) - 1):
+    for i in range(0, len(file_names) - 1, 2):
         frame1 = cv2.imread(os.path.join(args.path, file_names[i]))
         h, w = frame1.shape[:2]
         frame2 = cv2.imread(os.path.join(args.path, file_names[i + 1]))
@@ -51,4 +52,4 @@ if __name__ == '__main__':
         pred_flo = F.interpolate(flow, (h, w), mode='bilinear', align_corners=False)[0]
         pred_flo = computeImg(pred_flo.cpu().numpy(), verbose=True, savePath=os.path.join("result", path,
                                                                                          'predicted_flow' + str(
-                                                                                             i + 1) + '.png'))
+                                                                                             i//2 + 1) + '.png'))

gradient bug fixed

models.py
@@ -110,6 +110,18 @@ class PWC_Net(nn.Module):
         pass
 
 
+def generate_grid(B, H, W, device):
+    xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
+    yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
+    xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    grid = torch.cat((xx, yy), 1).float()
+    grid = torch.transpose(grid, 1, 2)
+    grid = torch.transpose(grid, 2, 3)
+    grid = grid.to(device)
+    return grid
+
+
 class Unsupervised(nn.Module):
     def __init__(self, conv_predictor="flownet"):
         super(Unsupervised, self).__init__()
@@ -121,33 +133,24 @@ class Unsupervised(nn.Module):
         else:
             self.predictor = FlowNetS()
 
-    def stn(self, flow, frame, grid):
-        H, W = frame.shape[2:]
-        flow = F.interpolate(flow, size=(H, W), mode='bilinear', align_corners=True)
+    def stn(self, flow, frame):
+        b, _, h, w = flow.shape
+        frame = F.interpolate(frame, size=(h, w), mode='bilinear', align_corners=True)
         flow = torch.transpose(flow, 1, 2)
         flow = torch.transpose(flow, 2, 3)
-        grid = flow + grid
-        xmax, xmin = torch.max(grid[:, :, :, 0]), torch.min(grid[:, :, :, 0])
-        ymax, ymin = torch.max(grid[:, :, :, 1]), torch.min(grid[:, :, :, 1])
-        factor = torch.FloatTensor([[[[2 / (xmax - xmin), 2 / (ymax - ymin)]]]]).to(frame.device)
-        shift = torch.FloatTensor([[[[xmin, ymin]]]]).to(frame.device)
-        grid = (grid - shift) * factor - 1
+
+        grid = flow + generate_grid(b, h, w, flow.device)
+
+        factor = torch.FloatTensor([[[[2 / w, 2 / h]]]]).to(flow.device)
+        grid = grid * factor - 1
         warped_frame = F.grid_sample(frame, grid)
+
         return warped_frame
 
     def forward(self, x):
-        B, C, H, W = x.size()
-        # mesh grid
-        xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
-        yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
-        xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
-        yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
-        grid = torch.cat((xx, yy), 1).float()
-        grid = torch.transpose(grid, 1, 2)
-        grid = torch.transpose(grid, 2, 3)
-        grid = grid.to(x.device)
+
         flow_predictions = self.predictor(x)
         frame2 = x[:, 3:, :, :]
-        warped_images = [self.stn(flow, frame2, grid) for flow in flow_predictions]
+        warped_images = [self.stn(flow, frame2) for flow in flow_predictions]
 
         return flow_predictions, warped_images

unsup_train.py
@@ -4,6 +4,7 @@ from dataset import *
 from models import Unsupervised
 from torch.utils.tensorboard import SummaryWriter
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 np.random.seed(seed=1)
@@ -50,7 +51,7 @@ def epoch(model, data, criterion, optimizer=None):
         imgs = imgs.to(device)
         with torch.set_grad_enabled(optimizer is not None):
             pred_flows, wraped_imgs = model(imgs)
-            loss, bce_loss, smooth_loss = unsup_loss(pred_flows, wraped_imgs, imgs[:, :3, :, :])
+            loss, bce_loss, smooth_loss = criterion(pred_flows, wraped_imgs, imgs[:, :3, :, :])
 
         if optimizer is not None:
             optimizer.zero_grad()
@@ -70,15 +71,15 @@ def epoch(model, data, criterion, optimizer=None):
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'smooth_loss {smooth.val:5.4f} ({smooth.avg:5.4f})\t'
                   'bce_loss {bce.val:5.4f} ({bce.avg:5.4f})'.format(
-                   "EVAL" if optimizer is None else "TRAIN", i, len(data), batch_time=avg_batch_time, loss=avg_loss,
-                   smooth=avg_smooth_loss, bce=avg_bce_loss))
+                "EVAL" if optimizer is None else "TRAIN", i, len(data), batch_time=avg_batch_time, loss=avg_loss,
+                smooth=avg_smooth_loss, bce=avg_bce_loss))
 
     print('\n===============> Total time {batch_time:d}s\t'
           'Avg loss {loss.avg:.4f}\t'
           'Avg smooth_loss {smooth.avg:5.4f} \t'
           'Avg bce_loss {bce.avg:5.4f} \n'.format(
-           batch_time=int(avg_batch_time.sum), loss=avg_loss,
-           smooth=avg_smooth_loss, bce=avg_bce_loss))
+        batch_time=int(avg_batch_time.sum), loss=avg_loss,
+        smooth=avg_smooth_loss, bce=avg_bce_loss))
 
     return avg_smooth_loss.avg, avg_bce_loss.avg, avg_loss.avg
 
@@ -86,7 +87,8 @@ def epoch(model, data, criterion, optimizer=None):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root', default= '../FlyingChairs_release/data', type=str, metavar='DIR', help='path to dataset')
+    parser.add_argument('--root', default='../FlyingChairs_release/data', type=str, metavar='DIR',
+                        help='path to dataset')
     parser.add_argument('--model', default='flownet', type=str, help='the supervised model to be trained with ('
                                                                      'flownet, lightflownet, pwc_net)')
     parser.add_argument('--steps', default=600000, type=int, metavar='N', help='number of total steps to run')
@@ -119,9 +121,9 @@ if __name__ == '__main__':
 
     if args.augment:
         if "Chairs" in args.root:
-            crop = albu.RandomSizedCrop((150, 384), 384, 512, w2h_ratio=512/384, p=0.5)
+            crop = albu.RandomSizedCrop((150, 384), 384, 512, w2h_ratio=512 / 384, p=0.5)
         elif "sintel" in args.root:
-            crop = albu.RandomSizedCrop((200, 436), 436, 1024, w2h_ratio=1024/436, p=0.5)
+            crop = albu.RandomSizedCrop((200, 436), 436, 1024, w2h_ratio=1024 / 436, p=0.5)
         else:
             crop = albu.RandomSizedCrop((200, 400), 250, 250, w2h_ratio=1, p=0.5)
         co_aug_transforms = albu.Compose([
@@ -200,7 +202,7 @@ if __name__ == '__main__':
         tb.add_scalars('smooth_loss', {"train": smooth_loss, "val": smooth_loss_val, "test": smooth_loss_test}, e)
         tb.add_scalars('bce_loss', {"train": bce_loss, "val": bce_loss_val, "test": bce_loss_test}, e)
 
-        if "Flying" in args.root and e >2:
-           if e % mile_stone == 0:
+        if "Flying" in args.root and e > 2:
+            if e % mile_stone == 0:
                 optim.param_groups[0]['lr'] *= 0.5
     tb.close()

utils.py
@@ -241,7 +241,6 @@ def EPE(flow_pred, flow_true, real=False):
     else:
         batch_size, _, h, w = flow_pred.shape
         flow_true = F.interpolate(flow_true, (h, w), mode='area')
-
     return torch.norm(flow_pred - flow_true, 2, 1).mean()
 
 
@@ -274,7 +273,7 @@ def evaluate(flow_pred, flow_true):
     return epe, aae
 
 
-def charbonnier(x, alpha=0.25, epsilon=1.e-3):
+def charbonnier(x, alpha=0.25, epsilon=1.e-9):
     return torch.pow(torch.pow(x, 2) + epsilon**2, alpha)
 
 
@@ -282,12 +281,17 @@ def smoothness_loss(flow):
     b, c, h, w = flow.size()
     v_translated = torch.cat((flow[:, :, 1:, :], torch.zeros(b, c, 1, w, device=flow.device)), dim=-2)
     h_translated = torch.cat((flow[:, :, :, 1:], torch.zeros(b, c, h, 1, device=flow.device)), dim=-1)
-    s_loss = charbonnier(flow - v_translated) + charbonnier(flow - v_translated)
-    return torch.sum(s_loss, dim=1).mean()
+    s_loss = charbonnier(flow - v_translated) + charbonnier(flow - h_translated)
+    s_loss = torch.sum(s_loss, dim=1) / 2
+    return torch.sum(s_loss)/b
 
 
 def photometric_loss(wraped, frame1):
-    return torch.sum(charbonnier(wraped - frame1), dim=1).mean()
+    h, w = wraped.shape[2:]
+    frame1 = F.interpolate(frame1, (h, w), mode='bilinear', align_corners=False)
+    p_loss = charbonnier(wraped - frame1)
+    p_loss = torch.sum(p_loss, dim=1)/3
+    return torch.sum(p_loss)/frame1.size(0)
 
 
 def unsup_loss(pred_flows, wraped_imgs, frame1, weights=(0.005, 0.01, 0.02, 0.08, 0.32)):